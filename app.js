cce84f8f67a70fb7355033cce207510e9e3aff3f
EhWanu <cameronjshadgett@gmail.com>
2022-02-02 11:35:58 -0500

    mapping is easy in vue

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const app = Vue.createApp({
 	data() {
 		return {
 			showBooks: true,
-			title: "The Final Empire",
-			author: "Brandon Sanderson",
-			age: 45,
-			x: 0,
-			y: 0,
+			books: [
+				{
+					title: "Name of The Wind",
+					author: "Patrick Rothfuss",
+				},
+				{
+					title: "The Way of Kings",
+					author: "Brandon Sanderson",
+				},
+				{
+					title: "The Final Empire",
+					author: "Brandon Sanderson",
+				},
+			],
 		};
 	},
 	methods: {
 		toggleShowBooks() {
 			this.showBooks = !this.showBooks;
-		},
-		handleEvent(e, data) {
-			console.log(e, e.type);
-			if (data) {
-				console.log(data);
-			}
-		},
-		handleMouseMove(e) {
-			this.x = e.offsetX;
-			this.y = e.offsetY;
 		},
 	},
 });
