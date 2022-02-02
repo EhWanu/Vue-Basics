153af176abee05ed8fd9c88ed8c02e0223ef1111
EhWanu <cameronjshadgett@gmail.com>
2022-02-02 12:09:28 -0500

    mapping data and images

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			url: "http://www.cameronshadgett.com",
 			showBooks: true,
 			books: [
 				{
 					title: "Name of The Wind",
 					author: "Patrick Rothfuss",
+					img: "assets/NameOfTheWind.jpeg",
 				},
 				{
 					title: "The Way of Kings",
 					author: "Brandon Sanderson",
+					img: "assets/TheFinalEmpire.jpeg",
 				},
 				{
 					title: "The Final Empire",
 					author: "Brandon Sanderson",
+					img: "assets/TheWayOfKings.jpeg",
 				},
 			],
 		};
